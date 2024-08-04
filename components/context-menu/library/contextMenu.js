33e1c94b9d2b7a52222965912f7a59e2368baa63
Ivan Teplov <iteplov7@gmail.com>
2024-08-04 16:10:17 +0200

    context-menu: move HTML helpers to the shared folder

--- components/context-menu/library/contextMenu.js
+++ components/context-menu/library/contextMenu.js
@@ -3,6 +3,7 @@
 // Licensed under the Apache license 2.0
 //
 
+import { findParentThat, getNextChildToFocusOnInsideOf } from "../../../shared/htmlHelpers.js"
 import { styleSheet } from "./styleSheet.js"
 
 if (document.adoptedStyleSheets.indexOf(styleSheet) === -1) {
@@ -345,78 +346,4 @@ export class ContextMenuGroupElement extends HTMLDetailsElement {
 		this.removeAttribute("data-x-expand-to")
 		this.removeAttribute("data-y-expand-to")
 	}
-}
-
-/**
- * Tries to find a parent element that meets the specified criteria
- * @param {(parent: HTMLElement) => boolean} meetsCriteria
- * @param {HTMLElement} child
- * @returns {HTMLElement|null}
- */
-function findParentThat(meetsCriteria, child) {
-	let parent = child?.parentElement
-
-	while (parent && !meetsCriteria(parent)) {
-		parent = parent?.parentElement
-	}
-
-	return parent
-}
-
-/**
- * Tells if the element is focusable
- * @param {HTMLElement} element
- * @returns {boolean}
- */
-function isFocusable(element) {
-	return !element.hasAttribute("disabled") && (
-		element instanceof HTMLButtonElement ||
-		element instanceof HTMLAnchorElement ||
-		element instanceof HTMLInputElement ||
-		element instanceof HTMLTextAreaElement ||
-		(element.getAttribute("tabindex") ?? "-1") !== "-1"
-	)
-}
-
-/**
- * Get the next child of the context menu.
- * Returns the top or bottom element when asking for the element
- * after the last one or before the first one respectively
- * @param {HTMLElement} parent
- * @param {HTMLElement} focusedElement
- * @param {boolean} isTheOneAfterCurrent
- */
-function getNextChildToFocusOnInsideOf(parent, focusedElement, isTheOneAfterCurrent) {
-	let nextToFocus
-
-	// If the focused element is currently outside of the menu
-	if (!parent.contains(focusedElement)) {
-		// Simply get the first or the last element
-		nextToFocus = isTheOneAfterCurrent
-			? parent.firstElementChild
-			: parent.lastElementChild
-	} else {
-		// Get the direct child of the parent instead of a probably deeply nested child
-		const directChild = focusedElement.parentElement !== parent
-			? findParentThat(element => element.parentElement === parent, focusedElement)
-			: focusedElement
-
-		// Get the next element after the direct child
-		nextToFocus = isTheOneAfterCurrent
-			? (directChild.nextElementSibling ?? parent.firstElementChild)
-			: (directChild.previousElementSibling ?? parent.lastElementChild)
-	}
-
-	// If the currently selected next element is not focusable or is not a submenu
-	while (!(isFocusable(nextToFocus) || nextToFocus instanceof ContextMenuGroupElement)) {
-		nextToFocus = isTheOneAfterCurrent
-			? (nextToFocus.nextElementSibling ?? parent.firstElementChild)
-			: (nextToFocus.previousElementSibling ?? parent.lastElementChild)
-	}
-
-	if (nextToFocus instanceof ContextMenuGroupElement) {
-		return nextToFocus.querySelector("summary")
-	}
-
-	return nextToFocus
 }
--- shared/htmlHelpers.js
+++ shared/htmlHelpers.js
@@ -11,6 +11,21 @@
 export const isFocused = element => document.activeElement === element
 
 /**
+ * Tells if the element is focusable
+ * @param {HTMLElement} element
+ * @returns {boolean}
+ */
+export function isFocusable(element) {
+	return !element.hasAttribute("disabled") && (
+		element instanceof HTMLButtonElement ||
+		element instanceof HTMLAnchorElement ||
+		element instanceof HTMLInputElement ||
+		element instanceof HTMLTextAreaElement ||
+		(element.getAttribute("tabindex") ?? "-1") !== "-1"
+	)
+}
+
+/**
  * Get object that contains touch position (depending on event type)
  * @param {MouseEvent|TouchEvent} event
  * @returns {MouseEvent|Touch}
@@ -39,6 +54,65 @@ export function getCSSVariableValue(element, variableName) {
  */
 export function elementContains(child, element) {
 	return element.contains(child) || element.shadowRoot?.contains(child)
+}
+
+/**
+ * Tries to find a parent element that meets the specified criteria
+ * @param {(parent: HTMLElement) => boolean} meetsCriteria
+ * @param {HTMLElement} child
+ * @returns {HTMLElement|null}
+ */
+export function findParentThat(meetsCriteria, child) {
+	let parent = child?.parentElement
+
+	while (parent && !meetsCriteria(parent)) {
+		parent = parent?.parentElement
+	}
+
+	return parent
+}
+
+/**
+ * Get the next child of the context menu.
+ * Returns the top or bottom element when asking for the element
+ * after the last one or before the first one respectively
+ * @param {HTMLElement} parent
+ * @param {HTMLElement} focusedElement
+ * @param {boolean} isTheOneAfterCurrent
+ */
+export function getNextChildToFocusOnInsideOf(parent, focusedElement, isTheOneAfterCurrent) {
+	let nextToFocus
+
+	// If the focused element is currently outside of the menu
+	if (!parent.contains(focusedElement)) {
+		// Simply get the first or the last element
+		nextToFocus = isTheOneAfterCurrent
+			? parent.firstElementChild
+			: parent.lastElementChild
+	} else {
+		// Get the direct child of the parent instead of a probably deeply nested child
+		const directChild = focusedElement.parentElement !== parent
+			? findParentThat(element => element.parentElement === parent, focusedElement)
+			: focusedElement
+
+		// Get the next element after the direct child
+		nextToFocus = isTheOneAfterCurrent
+			? (directChild.nextElementSibling ?? parent.firstElementChild)
+			: (directChild.previousElementSibling ?? parent.lastElementChild)
+	}
+
+	// If the currently selected next element is not focusable or is not a submenu
+	while (!(isFocusable(nextToFocus) || nextToFocus instanceof ContextMenuGroupElement)) {
+		nextToFocus = isTheOneAfterCurrent
+			? (nextToFocus.nextElementSibling ?? parent.firstElementChild)
+			: (nextToFocus.previousElementSibling ?? parent.lastElementChild)
+	}
+
+	if (nextToFocus instanceof ContextMenuGroupElement) {
+		return nextToFocus.querySelector("summary")
+	}
+
+	return nextToFocus
 }
 
 
